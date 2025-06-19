ef858eec618544667582c2e849d3d84b676d98a1
hwangdeokthien <huynhducthien41906@gmail.com>
2025-06-19 09:55:34 +0700

    [UPDATE] data-bin path

--- en-vi-transformer/eval_envi.sh
+++ en-vi-transformer/eval_envi.sh
@@ -7,12 +7,12 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # === Configuration ===
 SRC=en
 TGT=vi
-DATA_DIR=$SCRIPT_DIR/trans_data
-SPM_DATA_DIR=$SCRIPT_DIR/trans_data/spm
+DATA_DIR=$SCRIPT_DIR/trans_datad
+BIN_DATA_DIR=$SCRIPT_DIR/data-bin/envi-bpe
 CHECKPOINT_DIR=$SCRIPT_DIR/checkpoints/envi-transformer
 BEST_CHECKPOINT=$CHECKPOINT_DIR/checkpoint_best.pt
 
-OUT_DIR=output
+OUT_DIR=$SCRIPT_DIR/output
 SPLIT="test"  # You can change to valid, train, etc.
 
 mkdir -p $OUT_DIR
@@ -20,7 +20,7 @@ mkdir -p $OUT_DIR
 # === Generate translations using the trained model ===
 echo "Generating translations from the checkpoint..."
 
-fairseq-generate $DATA_DIR \
+fairseq-generate $BIN_DATA_DIR \
   --path $BEST_CHECKPOINT \
   --batch-size 128 --beam 5 --remove-bpe \
   --source-lang $SRC --target-lang $TGT \
