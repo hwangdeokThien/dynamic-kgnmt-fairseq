3a3dfab73eb57764fe48646657c6f4ce982fa3ea
hwangdeokthien <huynhducthien41906@gmail.com>
2025-06-24 22:11:02 +0700

    [UPDATE] prepare kg data

--- en-vi-dynamic-kgnmt/prepare_dynamic_kgnmt_envi.sh
+++ en-vi-dynamic-kgnmt/prepare_dynamic_kgnmt_envi.sh
@@ -64,6 +64,8 @@ fairseq-preprocess \
 mv $OUT_DIR/train.kg-kg.kg.bin $OUT_DIR/train.en-vi.kg.bin
 mv $OUT_DIR/valid.kg-kg.kg.bin $OUT_DIR/valid.en-vi.kg.bin
 mv $OUT_DIR/test.kg-kg.kg.bin $OUT_DIR/test.en-vi.kg.bin
-
+mv $OUT_DIR/train.kg-kg.kg.idx $OUT_DIR/train.en-vi.kg.idx
+mv $OUT_DIR/valid.kg-kg.kg.idx $OUT_DIR/valid.en-vi.kg.idx
+mv $OUT_DIR/test.kg-kg.kg.idx $OUT_DIR/test.en-vi.kg.idx
 
 echo "✅ Data preparation complete: $OUT_DIR"
