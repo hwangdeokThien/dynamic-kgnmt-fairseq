21b25eb0988513d0075b3515927c9ac652d3af39
hwangdeokthien <huynhducthien41906@gmail.com>
2025-06-24 21:31:54 +0700

    [ADD] preprocess kg data

--- en-vi-dynamic-kgnmt/prepare_dynamic_kgnmt_envi.sh
+++ en-vi-dynamic-kgnmt/prepare_dynamic_kgnmt_envi.sh
@@ -61,4 +61,8 @@ fairseq-preprocess \
   --joined-dictionary \
   --workers 4
 
+mv $OUT_DIR/train.kg-kg.kg.bin $OUT_DIR/train.en-vi.kg.bin
+mv $OUT_DIR/valid.kg-kg.kg.bin $OUT_DIR/valid.en-vi.kg.bin
+mv $OUT_DIR/test.kg-kg.kg.bin $OUT_DIR/test.en-vi.kg.bin
+
 echo "✅ Data preparation complete: $OUT_DIR"
