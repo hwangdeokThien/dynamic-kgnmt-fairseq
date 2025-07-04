154f549c13be29a71ed2d3f6543d680e3955a55f
hwangdeokthien <huynhducthien41906@gmail.com>
2025-07-04 21:17:21 +0700

    [UPDATE] loading from checkpoints

--- fairseq/checkpoint_utils.py
+++ fairseq/checkpoint_utils.py
@@ -607,11 +607,147 @@ def _torch_persistent_save(obj, f):
                 time.sleep(2.5)
 
 
-def _upgrade_state_dict(state):
+# def _upgrade_state_dict(state): # TODO_THESIS: some error about best_loss here
+#     """Helper for upgrading old model checkpoints."""
+
+#     # add optimizer_history
+#     if "optimizer_history" not in state:
+#         state["optimizer_history"] = [
+#             {"criterion_name": "CrossEntropyCriterion", "best_loss": state["best_loss"]}
+#         ]
+#         state["last_optimizer_state"] = state["optimizer"]
+#         del state["optimizer"]
+#         del state["best_loss"]
+#     # move extra_state into sub-dictionary
+#     if "epoch" in state and "extra_state" not in state:
+#         state["extra_state"] = {
+#             "epoch": state["epoch"],
+#             "batch_offset": state["batch_offset"],
+#             "val_loss": state["val_loss"],
+#         }
+#         del state["epoch"]
+#         del state["batch_offset"]
+#         del state["val_loss"]
+#     # reduce optimizer history's memory usage (only keep the last state)
+#     if "optimizer" in state["optimizer_history"][-1]:
+#         state["last_optimizer_state"] = state["optimizer_history"][-1]["optimizer"]
+#         for optim_hist in state["optimizer_history"]:
+#             del optim_hist["optimizer"]
+#     # record the optimizer class name
+#     if "optimizer_name" not in state["optimizer_history"][-1]:
+#         state["optimizer_history"][-1]["optimizer_name"] = "FairseqNAG"
+#     # move best_loss into lr_scheduler_state
+#     if "lr_scheduler_state" not in state["optimizer_history"][-1]:
+#         state["optimizer_history"][-1]["lr_scheduler_state"] = {
+#             "best": state["optimizer_history"][-1]["best_loss"]
+#         }
+#         del state["optimizer_history"][-1]["best_loss"]
+#     # keep track of number of updates
+#     if "num_updates" not in state["optimizer_history"][-1]:
+#         state["optimizer_history"][-1]["num_updates"] = 0
+#     # use stateful training data iterator
+#     if "train_iterator" not in state["extra_state"]:
+#         state["extra_state"]["train_iterator"] = {
+#             "epoch": state["extra_state"].get("epoch", 0),
+#             "iterations_in_epoch": state["extra_state"].get("batch_offset", 0),
+#         }
+
+#     # backward compatibility, cfg updates
+#     if "args" in state and state["args"] is not None:
+#         # old model checkpoints may not have separate source/target positions
+#         if hasattr(state["args"], "max_positions") and not hasattr(
+#             state["args"], "max_source_positions"
+#         ):
+#             state["args"].max_source_positions = state["args"].max_positions
+#             state["args"].max_target_positions = state["args"].max_positions
+#         # default to translation task
+#         if not hasattr(state["args"], "task"):
+#             state["args"].task = "translation"
+#         # --raw-text and --lazy-load are deprecated
+#         if getattr(state["args"], "raw_text", False):
+#             state["args"].dataset_impl = "raw"
+#         elif getattr(state["args"], "lazy_load", False):
+#             state["args"].dataset_impl = "lazy"
+#         # epochs start at 1
+#         if state["extra_state"]["train_iterator"] is not None:
+#             state["extra_state"]["train_iterator"]["epoch"] = max(
+#                 state["extra_state"]["train_iterator"].get("epoch", 1), 1
+#             )
+#         # --remove-bpe ==> --postprocess
+#         if hasattr(state["args"], "remove_bpe"):
+#             state["args"].post_process = state["args"].remove_bpe
+#         # --min-lr ==> --stop-min-lr
+#         if hasattr(state["args"], "min_lr"):
+#             state["args"].stop_min_lr = state["args"].min_lr
+#             del state["args"].min_lr
+#         # binary_cross_entropy / kd_binary_cross_entropy => wav2vec criterion
+#         if hasattr(state["args"], "criterion") and state["args"].criterion in [
+#             "binary_cross_entropy",
+#             "kd_binary_cross_entropy",
+#         ]:
+#             state["args"].criterion = "wav2vec"
+#         # remove log_keys if it's None (criteria will supply a default value of [])
+#         if hasattr(state["args"], "log_keys") and state["args"].log_keys is None:
+#             delattr(state["args"], "log_keys")
+#         # speech_pretraining => audio pretraining
+#         if (
+#             hasattr(state["args"], "task")
+#             and state["args"].task == "speech_pretraining"
+#         ):
+#             state["args"].task = "audio_pretraining"
+#         # audio_cpc => wav2vec
+#         if hasattr(state["args"], "arch") and state["args"].arch == "audio_cpc":
+#             state["args"].arch = "wav2vec"
+#         # convert legacy float learning rate to List[float]
+#         if hasattr(state["args"], "lr") and isinstance(state["args"].lr, float):
+#             state["args"].lr = [state["args"].lr]
+#         # convert task data arg to a string instead of List[string]
+#         if (
+#             hasattr(state["args"], "data")
+#             and isinstance(state["args"].data, list)
+#             and len(state["args"].data) > 0
+#         ):
+#             state["args"].data = state["args"].data[0]
+
+#         state["cfg"] = convert_namespace_to_omegaconf(state["args"])
+
+#     if "cfg" in state and state["cfg"] is not None:
+#         cfg = state["cfg"]
+#         with open_dict(cfg):
+#             # any upgrades for Hydra-based configs
+#             if (
+#                 "task" in cfg
+#                 and "eval_wer_config" in cfg.task
+#                 and isinstance(cfg.task.eval_wer_config.print_alignment, bool)
+#             ):
+#                 cfg.task.eval_wer_config.print_alignment = "hard"
+#             if "generation" in cfg and isinstance(cfg.generation.print_alignment, bool):
+#                 cfg.generation.print_alignment = (
+#                     "hard" if cfg.generation.print_alignment else None
+#                 )
+#             if (
+#                 "model" in cfg
+#                 and "w2v_args" in cfg.model
+#                 and cfg.model.w2v_args is not None
+#                 and (
+#                     hasattr(cfg.model.w2v_args, "task") or "task" in cfg.model.w2v_args
+#                 )
+#                 and hasattr(cfg.model.w2v_args.task, "eval_wer_config")
+#                 and cfg.model.w2v_args.task.eval_wer_config is not None
+#                 and isinstance(
+#                     cfg.model.w2v_args.task.eval_wer_config.print_alignment, bool
+#                 )
+#             ):
+#                 cfg.model.w2v_args.task.eval_wer_config.print_alignment = "hard"
+
+#     return state
+
+
+def _upgrade_state_dict(state): # TODO_THESIS: some error about best_loss here
     """Helper for upgrading old model checkpoints."""
 
     # add optimizer_history
-    if "optimizer_history" not in state:
+    if "optimizer_history" not in state and "kgnmt_optimizer_history" not in state and "knowledge_selector_optimizer_history" not in state:
         state["optimizer_history"] = [
             {"criterion_name": "CrossEntropyCriterion", "best_loss": state["best_loss"]}
         ]
@@ -629,22 +765,31 @@ def _upgrade_state_dict(state):
         del state["batch_offset"]
         del state["val_loss"]
     # reduce optimizer history's memory usage (only keep the last state)
-    if "optimizer" in state["optimizer_history"][-1]:
-        state["last_optimizer_state"] = state["optimizer_history"][-1]["optimizer"]
-        for optim_hist in state["optimizer_history"]:
+    if "optimizer" in state["kgnmt_optimizer_history"][-1] and "optimizer" in state["knowledge_selector_optimizer_history"][-1]:
+        state["last_kgnmt_optimizer_state"] = state["kgnmt_optimizer_history"][-1]["optimizer"]
+        state["last_knowledge_selector_optimizer_state"] = state["knowledge_selector_optimizer_history"][-1]["optimizer"]
+        for optim_hist in state["kngmt_optimizer_history"]:
+            del optim_hist["optimizer"]
+        for optim_hist in state["knowledge_selector_optimizer_history"]:
             del optim_hist["optimizer"]
     # record the optimizer class name
-    if "optimizer_name" not in state["optimizer_history"][-1]:
-        state["optimizer_history"][-1]["optimizer_name"] = "FairseqNAG"
+    if "optimizer_name" not in state["kgnmt_optimizer_history"][-1] and "optimizer_name" not in state["knowledge_selector_optimizer_history"][-1]:
+        state["kgnmt_optimizer_history"][-1]["optimizer_name"] = "FairseqNAG"
+        state["knowledge_selector_optimizer_history"][-1]["optimizer_name"] = "FairseqNAG"
     # move best_loss into lr_scheduler_state
-    if "lr_scheduler_state" not in state["optimizer_history"][-1]:
-        state["optimizer_history"][-1]["lr_scheduler_state"] = {
-            "best": state["optimizer_history"][-1]["best_loss"]
+    if "lr_scheduler_state" not in state["kgnmt_optimizer_history"][-1] and "lr_scheduler_state" not in state["knowledge_selector_optimizer_history"][-1]:
+        state["kgnmt_optimizer_history"][-1]["lr_scheduler_state"] = {
+            "best": state["kgnmt_optimizer_history"][-1]["best_loss"]
         }
-        del state["optimizer_history"][-1]["best_loss"]
+        del state["kgnmt_optimizer_history"][-1]["best_loss"]
+        state["knowledge_selector_optimizer_history"][-1]["lr_scheduler_state"] = {
+            "best": state["knowledge_selector_optimizer_history"][-1]["best_loss"]
+        }
+        del state["knowledge_selector_optimizer_history"][-1]["best_loss"]
     # keep track of number of updates
-    if "num_updates" not in state["optimizer_history"][-1]:
-        state["optimizer_history"][-1]["num_updates"] = 0
+    if "num_updates" not in state["kgnmt_optimizer_history"][-1] and "num_updates" not in state["knowledge_selector_optimizer_history"][-1]:
+        state["kgnmt_optimizer_history"][-1]["num_updates"] = 0
+        state["knowledge_selector_optimizer_history"][-1]["num_updates"] = 0
     # use stateful training data iterator
     if "train_iterator" not in state["extra_state"]:
         state["extra_state"]["train_iterator"] = {
@@ -741,7 +886,6 @@ def _upgrade_state_dict(state):
                 cfg.model.w2v_args.task.eval_wer_config.print_alignment = "hard"
 
     return state
-
 
 def prune_state_dict(state_dict, model_cfg: Optional[DictConfig]):
     """Prune the given state_dict if desired for LayerDrop
